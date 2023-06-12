f4d80ceaa67ae47a7f2c51bbe791abb977af8980
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-12 15:23:35 -0500

    correctly implement react router dom in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +12,6 @@ root.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
