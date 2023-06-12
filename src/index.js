4d0f0ceb3d79fcadb725b3d95969abe8ca3f4d8f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-12 15:19:09 -0500

    add browser router to app.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
