32a893a0998f2c166d31d742ec36634617340a5b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-12 15:41:28 -0500

    add switch and route to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+/////////////////////////////////////////////
+// App.js
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World Val</h1>
-    </div>
+    <switch>
+      <Route exact path="/">
+        <div className="App">
+          <h1>Hello World Val</h1>
+          <p>2306120340</p>
+        </div>
+      </Route>
+    </switch>
   );
 }
 
